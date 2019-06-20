ea5eff8fedfc12c8669b554bd46b9dc3703c11ac
Demetri Pananos <dpananos@gmail.com>
2019-06-20 17:14:53 -0400

    Sampling Model in .py

--- Scripts/ode_api.py
+++ Scripts/ode_api.py
@@ -7,14 +7,16 @@ THEANO_FLAGS='optimizer=fast_compile'
 
 class ODEModel(object):
 
-    def __init__(self, odefunc, n_states, n_ivs, n_odeparams, y0 = None):
+    def __init__(self, odefunc, n_states, n_ivs, n_odeparams):
 
         self.odefunc = odefunc
         self.n_states = n_states
         self.n_ivs = n_ivs
         self.n_odeparams = n_odeparams
-        self.y0 = y0
-        self.augmented_system = augment_system(self.odefunc)
+        self.augmented_system = _augment_system(self.odefunc)
+
+        self.n = self.n_states
+        self.m = self.n_odeparams + self.n_ivs
 
     def system(self,Y,t,p):
 
@@ -32,19 +34,19 @@ class ODEModel(object):
         """
 
 
-        dydt, sens = self.augmented_system(Y[:self.n_states], 
+        dydt, sens = self.augmented_system(Y[:self.n], 
                                             t,
                                             p,
-                                            Y[self.n_states:],
-                                            self.n_states,
-                                            self.n_odeparams + self.n_ivs
+                                            Y[self.n:],
+                                            self.n,
+                                            self.m
                                             )
 
         derivatives =  np.concatenate([dydt,sens])
 
         return derivatives
 
-    def simulate(self, times, parameters):
+    def simulate(self, parameters, times):
 
         '''
         This function returns solutions and sensitivities of the ODE,
@@ -59,32 +61,81 @@ class ODEModel(object):
             sol(array): Solution of ODE
         '''
 
+        #Set up the inital condition for the sensitivities
+        sens_ic = np.zeros((self.n, self.m))
 
-        sens_ic = np.zeros((self.n_states, self.n_odeparams + self.n_ivs))
+        #Last n columns correspond to senstivity for inital condition
+        #So last n columns form identity matrix
+        sens_ic[:, -self.n:] = np.eye(self.n)
 
-        sens_ic[:, -self.n_states:] = np.eye(self.n_states)
+        #Create an initial condition for the system
+        y_ic = parameters[-self.n:]
 
-        y_ic = parameters[-self.n_states:]
-
+        #Concatenate the two inital conditions to form the initial condition
+        #for the augmented system (ODE + sensitivity ODe)
         y0 = np.concatenate([y_ic, sens_ic.ravel()])
 
-
+        #Integrate
         soln = scipy.integrate.odeint(self.system,
                     y0=y0,
                     t = times,
                     args = tuple([parameters]))
 
-        y = soln[:, :self.n_states]
-        sens = soln[:, self.n_states:].reshape((len(times),self.n_states, self.n_odeparams + self.n_ivs) )
+        #Reshaoe the sensitivities so that there is an nxm matrix for each 
+        #timestep
+        y = soln[:, :self.n]
+        sens = soln[:, self.n:].reshape((len(times),self.n, self.m) )
 
         return [y,sens]
 
+class ODEGradop(theano.Op):
+    def __init__(self, numpy_vsp):
+        self._numpy_vsp = numpy_vsp
+
+    def make_node(self, x, g):
+        x = theano.tensor.as_tensor_variable(x)
+        g = theano.tensor.as_tensor_variable(g)
+        node = theano.Apply(self, [x, g], [g.type()])
+        return node
+
+    def perform(self, node, inputs_storage, output_storage):
+        x = inputs_storage[0]
+
+        g = inputs_storage[1]
+        out = output_storage[0]
+        out[0] = self._numpy_vsp(x, g)       # get the numerical VSP
+        
+class ODEop(theano.Op):
+
+    def __init__(self, state, numpy_vsp):
+        self._state = state
+        self._numpy_vsp = numpy_vsp
+
+    def make_node(self, x):
+        x = theano.tensor.as_tensor_variable(x)
+
+        return theano.Apply(self, [x], [x.type()])
+
+    def perform(self, node, inputs_storage, output_storage):
+        x = inputs_storage[0]
+        out = output_storage[0]
+        
+        out[0] = self._state(x)               # get the numerical solution of ODE states
+
+    def grad(self, inputs, output_grads):
+        x = inputs[0]
+        g = output_grads[0]
+
+        grad_op = ODEGradop(self._numpy_vsp)  # pass the VSP when asked for gradient 
+        grad_op_apply = grad_op(x, g)
+        
+        return [grad_op_apply]
 
 
 
-def augment_system(ode_func):
 
 
+def _augment_system(ode_func):
     '''Function to create augmented system.
 
     Take a function which specifies a set of differential equations and return
@@ -135,9 +186,10 @@ def augment_system(ode_func):
     #This is the time derivative of dydp
     ddt_dydp = (Jdfdy + grad_f).flatten()
 
-    #TODO: Ensure system is reshaped on the odeint call?
+
     system = theano.function(
             inputs=[t_y, t_t, t_p, dydp_vec, t_n, t_m],
-            outputs=[f_tensor, ddt_dydp],on_unused_input='ignore')
+            outputs=[f_tensor, ddt_dydp],
+            on_unused_input='ignore')
 
     return system
--- Scripts/test.py
+++ Scripts/test.py
@@ -3,24 +3,80 @@ import numpy as np
 from ode_api import *
 from ode_funcs import *
 import matplotlib.pyplot as plt
-
-
-n_states = 2
-n_params = 2
-y0 = np.array([0.99,.01])
-p0 = np.zeros(n_states*n_params)
+import scipy
+import pymc3 as pm
 
 #odefunc, n_states, n_ivs, n_odeparams, y0 = None
-model = ODEModel(test_ode_func_4, 
-                2,
-                2,
-                2,
-                y0 = np.concatenate([y0,p0]))
+
+n_states = 2
+n_ivs = 2
+n_odeparams = 1
+model = ODEModel(odefunc = test_ode_func_3, 
+                    n_states = n_states,
+                    n_ivs = n_ivs,
+                    n_odeparams = n_odeparams)
 
 
-times = np.linspace(0,5,1001)
-parameters = [8,1, 0.99, 0.01]
+class solveCached(object):
+    def __init__(self, times, n_params, n_outputs):
+      
+        self._times = times
+        self._n_params = n_params
+        self._n_outputs = n_outputs
+        self._cachedParam = np.zeros(n_params)
+        self._cachedSens = np.zeros((len(times), n_outputs, n_params))
+        self._cachedState = np.zeros((len(times),n_outputs))
+        
+    def __call__(self, x):
+        
+        if np.all(x==self._cachedParam):
+            state, sens = self._cachedState, self._cachedSens
+            
+        else:
+            state, sens = model.simulate(x, times)
+        
+        return state, sens
 
-y,sens = model.simulate(times, parameters)
 
-print(sens[0])
+times = np.arange(0,10) # number of measurement points (see below)   
+cached_solver=solveCached(times, n_odeparams + n_ivs, n_states)
+
+def state(x):
+    State, Sens = cached_solver(np.array(x,dtype=np.float64))
+    cached_solver._cachedState, cached_solver._cachedSens, cached_solver._cachedParam = State, Sens, x
+    return State.reshape((2*len(State),))
+
+def numpy_vsp(x, g):    
+    numpy_sens = cached_solver(np.array(x,dtype=np.float64))[1].reshape((n_states*len(times),len(x)))
+    return numpy_sens.T.dot(g)
+
+
+sims, sens = model.simulate([8,0.99,0.01], times)
+S = scipy.stats.lognorm.rvs(s = 0.1, scale = sims[:,0])
+I = scipy.stats.lognorm.rvs(s = 0.1, scale = sims[:,1])
+
+# Define the data matrix
+Y = np.vstack((S,I)).T
+
+# Now instantiate the theano custom ODE op
+my_ODEop = ODEop(state,numpy_vsp)
+
+# The probabilistic model
+with pm.Model() as LV_model:
+
+    # Priors for unknown model parameters
+    beta = pm.Normal('beta',6,2)
+    # S0 = pm.Deterministic('S0',0.99)
+    # I0 = pm.Deterministic('I0',0.01)
+    
+    sigma = pm.HalfCauchy('sigma',1)
+
+    # Forward model
+    all_params = pm.math.stack([beta, 0.99,0.01],axis=0)
+    ode_sol = my_ODEop(all_params)
+    forward = ode_sol.reshape(Y.shape)
+
+    # Likelihood 
+    Y_obs = pm.Lognormal('Y_obs', mu=pm.math.log(forward), sd=sigma, observed=Y)
+    
+    trace = pm.sample(1500, tune=1000, init='adapt_diag')
